bda30a5a2664aa3e066179899f3d27c859fc8032
wanglin <wanglin46@lenovo.com>
2021-04-26 10:17:49 +0800

    webpack_import init

--- flow/src/index.js
+++ flow/src/index.js
@@ -1,3 +1,14 @@
-import name, { age } from './title';
-console.log(name);
-console.log(age);
+let importBtn = document.getElementById('import');
+importBtn.addEventListener('click', () => {
+  // result就是这个title模块的导出对象
+  import(/* webpackChunkName: 'title' */ './title').then(result => {
+    console.log(result)
+  })
+})
+
+
+// import('./title')
+
+// __webpack_require__.e('title').then(__webpack_require__.t.bind(null, './src/title.js', 7)).then((result) => {
+//   console.log(result);
+// });
